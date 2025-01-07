11fa498f1b4559cbe7be144b36b47e1c3c347edb
Arjohn Ely <arjohn.ely45@gmail.com>
2025-01-07 21:54:18 +0800

    initial commit

---
+++ routes/api.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::group(['namespace' => 'App\Http\Controllers'], function () {
+    Route::group(['prefix' => 'auth'], function () {
+        Route::post('login', 'AuthController@login');
+        Route::post('register', 'AuthController@register');
+        Route::post('forgot-password', 'AuthController@forgotPassword')->middleware('guest')->name('password.email');
+        Route::post('reset-password', 'AuthController@resetPassword')->middleware('guest')->name('password.reset');
+    });
+
+    Route::post('auth/resend-verify-email', 'AuthController@resendVerifyEmail')->middleware(['throttle:6,1']);
+    //for authenticated
+    Route::group(['middleware' => 'auth:sanctum'], function () {
+        Route::group(['prefix' => 'auth'], function () {
+            Route::post('verify-email', 'AuthController@verifyEmail');
+            Route::post('logout', 'AuthController@logout');
+            Route::post('verify-token', 'AuthController@verifyToken');
+            Route::post('change-password', 'AuthController@changePassword');
+        });
+    });
+});
+
+
